33b9972c04f2ec9385a43c67f5625f332a595fc2
flwkemd <flwkemd@naver.com>
2017-11-08 13:22:35 +0100

    implemented basic board

--- src/main/java/com/example/demo/JspTest.java
+++ src/main/java/com/example/demo/JspTest.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.board.mapper.BoardMapper;
 
 @Controller
 public class JspTest {
 
-	@RequestMapping("/test")
-	private String jspTest() {
-		return "test";// 생성한 jsp명 (test.jsp)
+	 @Resource(name="com.example.demo.board.mapper.BoardMapper")
+	    BoardMapper mBoardMapper;
+	    
+	    @RequestMapping("/test")
+	    private String jspTest() throws Exception{
+	        
+	        System.out.println(mBoardMapper.boardCount()); // <<<<TEST
+	        
+	        return "test"; 
+
 	}
 }
